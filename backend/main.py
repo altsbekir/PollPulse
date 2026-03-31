01b18e9e4167fb0c86623b9b20545e405bb2e156
Bekir Alataş <bekiralatas2332@gmail.com>
2026-03-31 07:44:12 +0300

    Fix: CORS URL'si birebir eşleştirildi

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://poll-pulse-tan.vercel.app" # BURAYA KENDİ VERCEL LİNKİNİ YAPIŞTIR (Sonunda / olmadan)
+        "https://poll-pulse-altsbekirs-projects.vercel.app" # BURAYA KENDİ VERCEL LİNKİNİ YAPIŞTIR (Sonunda / olmadan)
     ],
     allow_credentials=True,
     allow_methods=["*"],
